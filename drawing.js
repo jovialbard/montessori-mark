8681d8d72f96fed71b206d6bf37dd38c439ed4c4
Mike Lucek <michael.s.lucek@gmail.com>
2015-08-29 17:15:19 -0400

    add letter to canvas. closes #1

--- drawing.js
+++ drawing.js
@@ -3,17 +3,34 @@ $(document).ready(function(){
 	var ctx = el.getContext('2d');
 	var isDrawing;
 
-	el.onmousedown = function(e) {
-	  isDrawing = true;
-	  ctx.moveTo(e.clientX-this.offsetLeft, e.clientY-this.offsetTop);
-	};
-	el.onmousemove = function(e) {
-	  if (isDrawing) {
-	    ctx.lineTo(e.clientX-this.offsetLeft, e.clientY-this.offsetTop);
-	    ctx.stroke();
-	  }
-	};
-	el.onmouseup = function() {
-	  isDrawing = false;
-	};
+	draw();
+	text();
+
+		function draw(){
+			el.onmousedown = function(e) {
+			  isDrawing = true;
+			  ctx.moveTo(e.clientX-this.offsetLeft, e.clientY-this.offsetTop);
+			};
+			el.onmousemove = function(e) {
+			  if (isDrawing) {
+			    ctx.lineTo(e.clientX-this.offsetLeft, e.clientY-this.offsetTop);
+			    ctx.stroke();
+			  }
+			};
+			el.onmouseup = function() {
+			  isDrawing = false;
+			}
+		}
+
+		function text(){
+			alert(el.width);
+			ctx.font = "300px sans-serif";
+			ctx.fillStyle = "red";
+			ctx.textAlign = "center";
+			ctx.fillText("A", el.width/2, 300); 
+		};
+
+
+
+	
 });
