7b910ff428e8a0b6ee6861b86725e07e625593f5
Mike Lucek <michael.s.lucek@gmail.com>
2015-08-29 18:12:44 -0400

    drawn line capped and smoothed

--- drawing.js
+++ drawing.js
@@ -7,12 +7,16 @@ $(document).ready(function(){
 	text();
 
 		function draw(){
+			//this.offsetLeft and this.offsetTop are needed to correct for the 
+			//position of the canvas on the page
 			el.onmousedown = function(e) {
 			  isDrawing = true;
 			  ctx.moveTo(e.clientX-this.offsetLeft, e.clientY-this.offsetTop);
 			};
 			el.onmousemove = function(e) {
 			  if (isDrawing) {
+			  	ctx.lineWidth = 10;
+			  	ctx.lineJoin = ctx.lineCap = "round";
 			    ctx.lineTo(e.clientX-this.offsetLeft, e.clientY-this.offsetTop);
 			    ctx.stroke();
 			  }
